7d0b77d128cddc5ed1a7152b37a4eb897be5f48e
Alexandre <alexandremimoso08@gmail.com>
2026-02-03 19:52:15 +0000

    Adds requirements for the program, as well as the program itself and the database in which it acts

--- juros/juros.py
+++ juros/juros.py
@@ -1,1 +1,11 @@
-print("HI")
+import pandas as pd
+from pathlib import Path
+
+# Define o caminho (path) para chegar à tabela excel
+path = str(Path(__file__).parent)
+
+df = pd.read_excel(path + '\\tabela.xlsx')
+df['Cf'] = df['C0'] * (1 + df['Taxa'] / 100)**df['Periodo']
+df.to_excel(path + '\\tabela.xlsx', index=False)
+
+print(df)
